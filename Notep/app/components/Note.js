5fb5d820aa5697af173d5ed8698e5722c63556e5
takaruangs <imtaka.kr@gmail.com>
2017-06-22 10:06:30 +0700

    hawk

---
+++ Notep/app/components/Note.js
@@ -0,0 +1,59 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+
+export default class Notep extends Component {
+  render() {
+    return (
+      <View key={this.props.keyval} style={styles.note}>
+
+        <Text style={styles.noteText}>{this.props.val.date}</Text>
+        <Text style={styles.noteText}>{this.props.val.note}</Text>
+
+        <TouchableOpacity onPress={this.props.deleteMethod} style={styles.noteDelete}>
+          <Text style={styles.noteDeleteText}>X</Text>
+        </TouchableOpacity>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  note:{
+      position: 'relative',
+      padding: 20,
+      paddingRight: 100,
+      borderBottomWidth: 2,
+      borderBottomColor: '#ededed',
+  },
+  noteText:{
+    paddingLeft: 20,
+    borderLeftWidth: 10,
+    borderLeftColor: '#E91E63',
+  },
+  noteDelete:{
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#DC143C',
+    padding: 10,
+    top: 10,
+    bottom: 10,
+    right: 10,
+  },
+  noteDeleteText:{
+    color: 'white',
+  }
+});
---
+++ Notep/index.android.js
@@ -0,0 +1,125 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+import Note from './app/components/Note';
+
+
+export default class Notep extends Component {
+
+  state = {
+    noteArray: [ {'date': 'testdate', 'note': 'testnote 1'} ],
+    noteText: '',
+  }
+
+  render() {
+
+    let notes = this.state.noteArray.map((val, key) => {
+      return <Note key={key} keyval={key} val={val} deleteMethod={ ()=>this.deleteNote(key) } />
+    });
+
+    return (
+      <View style={styles.container}>
+
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Red Comet Note</Text>
+        </View>
+
+        <ScrollView style={styles.scrollContainer}>
+          {notes}
+        </ScrollView>
+
+        <View style={styles.footer}>
+
+          <TouchableOpacity onPress={this.addNote.bind(this)} style={styles.addButton}>
+            <Text style={styles.addButtonText}>+</Text>
+          </TouchableOpacity>
+
+          <TextInput style={styles.textInput}
+              onChangeText={(noteText) => this.setState({noteText})} value={this.state.noteText}
+              placeholder='Add some note here...' placeholderTextColor='white' underlineColorAndroid='transparent'>
+          </TextInput>
+        </View>
+
+      </View>
+    );
+  }
+  addNote() {
+    if (this.state.noteText){
+       var d = new Date();
+       this.state.noteArray.push( {'date': d.getFullYear() + "/" + (d.getMonth() +1) + "/" + d.getDate(), 'note': this.state.noteText} );
+       this.setState({ noteArray: this.state.noteArray })
+       this.setState({ noteText: '' });
+    }
+  }
+
+  deleteNote(key){
+    this.state.noteArray.splice(key, 1);
+    this.setState({ noteArray: this.state.noteArray });
+  }
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    backgroundColor: '#FF0000',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    borderBottomWidth: 10,
+    borderBottomColor: '#F08080',
+  },
+  headerText:{
+    color: 'white',
+    fontSize: 30,
+    padding: 26,
+  },
+  scrollContainer:{
+    flex: 1,
+    marginBottom: 100,
+  },
+  footer:{
+    position: 'absolute',
+    alignItems: 'center',
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  addButton:{
+    backgroundColor: '#FF4500',
+    width: 90,
+    height: 90,
+    borderRadius: 50,
+    borderColor: '#ccc',
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 8,
+    marginBottom: -45,
+    zIndex: 10,
+  },
+  addButtonText:{
+    color: '#fff',
+    fontSize: 24,
+  },
+  textInput:{
+    alignSelf: 'stretch',
+    color:'#fff',
+    padding: 20,
+    paddingTop: 46,
+    backgroundColor: '#252525',
+    borderTopWidth: 2,
+    borderTopColor: '#FF0000',
+  }
+});
+
+AppRegistry.registerComponent('Notep', () => Notep);
